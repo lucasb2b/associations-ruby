3747518389d43d982e33a6d603a79c9dff985856
Lucas Brito de Lima <lucasbritocientista@gmail.com>
2023-08-03 21:18:04 -0300

    Associations: has_may through

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_01_021425) do
+ActiveRecord::Schema.define(version: 2023_08_03_235404) do
 
   create_table "accounts", force: :cascade do |t|
     t.integer "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_accounts_on_author_id"
+  end
+
+  create_table "appointments", force: :cascade do |t|
+    t.integer "doctor_id"
+    t.integer "patient_id"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
+    t.index ["patient_id"], name: "index_appointments_on_patient_id"
   end
 
   create_table "authors", force: :cascade do |t|
@@ -31,6 +41,18 @@ ActiveRecord::Schema.define(version: 2023_08_01_021425) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["author_id"], name: "index_books_on_author_id"
+  end
+
+  create_table "doctors", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "patients", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+d1 = Doctor.create(name: "Estranho 2")
+d2 = Doctor.create(name: "Banner 2")
+
+p1 = Patient.create(name: "João")
+p2 = Patient.create(name: "Maria")
+
+Appointment.create(doctor: d1, patient: p1, date: Date.today)
+Appointment.create(doctor: d2, patient: p2, date: Date.today)
+Appointment.create(doctor: d1, patient: p2, date: Date.today)
+Appointment.create(doctor: d2, patient: p1, date: Date.today)
+
